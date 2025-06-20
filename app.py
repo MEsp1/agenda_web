a3bd5f274e7208bd103548dfe259c385d2975167
MEsp1 <1724110748@utectulancingo.edu.mx>
2025-06-20 19:39:44 +0000

    No le muevas bro

--- app.py
+++ app.py
@@ -3,51 +3,29 @@ import sqlite3
 
 urls = (
     "/", "Personas",
-    "/detalle", "Detalle",
-    "/editar", "Editar",
-    "/borrar", "Borrar"
+    "/detalle/(.*)", "Detalle",
+    "/editar/(.*)", "Editar",
+    "/borrar/(.*)", "Borrar"
 )
 
 app = web.application(urls, globals())
 render = web.template.render("views/")
 
-
 class Personas:
     def GET(self):
-        try:
-            conection = sqlite3.connect("agenda.db")
-            cursor = conection.cursor()
-            result = cursor.execute("select * from personas")
-            return render.personas(result)
-        except Exception as e:
-            print(f"Error001:{e}")
-            return f"No se puedo conectar a la base de datos"
+        conection = sqlite3.connect("agenda.db")
+        cursor = conection.cursor()
+        result = cursor.execute("SELECT * FROM personas")
+        return render.personas(result)
 
     def POST(self):
         form = web.input()
         conection = sqlite3.connect("agenda.db")
         cursor = conection.cursor()
-        cursor.execute(
-            "insert into personas(nombre,email) values (?, ?)",
-            (form.nombre, form.email),
-        )
+        cursor.execute("INSERT INTO personas (nombre, email) VALUES (?, ?)", (form.nombre, form.email))
         conection.commit()
         conection.close()
         raise web.seeother("/")
-
-
-class Editar:
-    def GET(self, id):
-        conection = sqlite3.connect("agenda.db")
-        cursor = conection.cursor()
-        cursor.execute("select * from personas where id=?", (id,))
-        persona = cursor.fetchone()
-        conection.close()
-        if persona:
-            return render.editar(persona)
-        else:
-            return "Persona no encontrada"
-
 
 if __name__ == "__main__":
     app.run()
